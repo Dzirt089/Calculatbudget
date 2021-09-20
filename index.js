5bf6b3970c9c12c9658343b81e7de87a1fc0a95a
Ilya Emets <suoza089@gmail.com>
2021-09-20 14:47:41 +0300

    добавлен второй цикл в программу

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ for (let i=0; i < 2; i++) {
 
 
 
-    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+    if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
         && a != '' && b != '' && a.length < 50 ) {
         console.log('done');
         appData.expenses[a] = b;
@@ -27,7 +27,28 @@ for (let i=0; i < 2; i++) {
         b = prompt ("Во сколько обойдется?", "");
     }
     
-};
+ }
+
+
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+        
+//     if( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log('bad result');
+//         i--;
+//     }
+
+//     i++;
+// }
+
+
+
 
 appData.moneyPerDay = appData.budget/30;
 
@@ -42,6 +63,6 @@ if(appData.moneyPerDay < 100) {
     console.log("Высокий уровень достатка");
 } else {
     console.log("Произошла ошибка");
-};
+}
 
 
